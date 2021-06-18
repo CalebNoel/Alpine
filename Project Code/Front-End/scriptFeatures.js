abffd8e9b563cb132d802e56b4eaebaff09876a2
justin10923 <jllee293@gmail.com>
2021-06-18 10:23:48 -0600

    Added JS, Added Log-in and About, Added Jumbotron to Home

---
+++ Project Code/Front-End/scriptFeatures.js
@@ -0,0 +1,53 @@
+/* To do: Add some navigation features here that make the site flow better */
+
+
+function makeInvisible(id)
+{
+    document.getElementById("student_status").style.visibility = 'hidden'
+    document.getElementById("student_status").style.height = 'auto'
+}
+
+function viewStudentStats(id, toggle)
+{
+	if(id=='student_status')
+	{
+		if(toggle =='1')
+		{
+			document.getElementById("student_status").style.visibility = 'visible'
+			document.getElementById("student_status").style.height = 'auto'
+		}
+		else
+		{
+			document.getElementById("student_status").style.visibility = 'hidden'
+			document.getElementById("undergrad_select").style.visibility = 'hidden'
+			document.getElementById("grad_select").style.visibility = 'hidden'
+			document.getElementById("student_status").style.height = '0px'
+		}
+	}
+	else if(id =='undergrad_select')
+	{
+		if(toggle =='1')
+		{
+			document.getElementById("undergrad_select").style.visibility= 'visible'
+			document.getElementById("undergrad_select").style.height = 'auto'
+		}
+		else
+		{
+			document.getElementById("undergrad_select").style.visibility = 'hidden'
+			document.getElementById("undergrad_select").style.height = '0px'
+		}
+	}
+	else if(id =='grad_select')
+	{
+		if(toggle =='1')
+		{
+			document.getElementById("grad_select").style.visibility= 'visible'
+			document.getElementById("grad_select").style.height = 'auto'
+		}
+		else
+		{
+			document.getElementById("grad_select").style.visibility = 'hidden'
+			document.getElementById("grad_select").style.height = '0px'
+		}
+	}
+} 

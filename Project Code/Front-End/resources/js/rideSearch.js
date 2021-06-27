2999d40ed93c560e093e4446f403401074a96426
lys0l <dali9115@colorado.edu>
2021-06-27 10:47:40 -0600

    Dates input validation

--- Project Code/Front-End/resources/js/rideSearch.js
+++ Project Code/Front-End/resources/js/rideSearch.js
@@ -1,4 +1,17 @@
+/*
 $(document).ready(function(){
  $('#start_date').datepicker();
  $('#end_date').datepicker();
 });
+*/
+
+function submitRideSearch() {
+  let start = new Date(document.getElementById('start_date').value);
+  let end = new Date(document.getElementById('end_date').value);
+  if( start <= end) {
+    //Proceed with search
+  }
+  else if (!isNaN(start.getDate()) && !isNaN(end.getDate())) {
+    alert('Start date cannot follow end date.');
+  }
+}
